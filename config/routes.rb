0d96a19d1f3aa51ec666dd7eddeffca8c6e79284
Brady Berryessa <Brady@MacBook-Pro.ad.uvu.edu>
2017-12-04 11:25:19 -0700

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+
+  #to: calls the controller#action and the as: changes the name of the prefix in rake routes
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :blogs
 
-  # for a root directory, you always have to use the pound sign
+  # for a 'to' directory, you always have to use the pound sign
   root to: 'pages#home'
 
 end
