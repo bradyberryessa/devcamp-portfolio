301b412c06b62099a0d440cb45af639539a883b9
Brady Berryessa <Brady@MacBook-Pro.ad.uvu.edu>
2017-12-05 10:18:10 -0700

    Implemented validations for blogs, skills, and portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,4 @@
 class Portfolio < ApplicationRecord
+#validates_presence_of is a rails method that doesn't allow empty field entries within a title, body, main_image, and thumb_image
+validates_presence_of :title, :body, :main_image, :thumb_image
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,4 @@
 class Skill < ApplicationRecord
+  #validates_presence_of is a rails method that doesn't allow empty field entries within a title and percent utilized
+  validates_presence_of :title, :percent_utilized
 end
