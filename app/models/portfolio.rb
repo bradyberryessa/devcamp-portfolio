24a11e7d04875261ac8fa8f89eed6c37cf4151f4
Brady Berryessa <you@example.com>
2017-12-18 11:37:59 -0700

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,6 +7,9 @@ class Portfolio < ApplicationRecord
 #validates_presence_of is a rails method that doesn't allow empty field entries within a title, body, main_image, and thumb_image
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  mount_uploader :thumb_image, PortfolioUploader 
+  mount_uploader :main_image, PortfolioUploader 
+
 #both of these are accomplish the same thing but one just uses a method to call and the other uses the scope method. Both can be tagged on to Portfolio#scope to the portfolios_controller
   def self.angular
     where(subtitle: "Angular")
